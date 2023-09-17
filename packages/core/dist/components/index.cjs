18e68337fd8e90de296571dc87a2e2964b3d2540
Byron <byronogis@outlook.com>
2023-09-17 15:52:00 +0800

    chore: gitignore dist

--- packages/core/dist/components/index.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const components_index = require('../shared/web-component-starter.1241acfc.cjs');
-require('lit');
-
-
-
-exports.WebComponentStarter = components_index.WebComponentStarter;
--- packages/core/dist/components/index.mjs
+++
@@ -1,2 +0,0 @@
-export { W as WebComponentStarter } from '../shared/web-component-starter.ff249175.mjs';
-import 'lit';
